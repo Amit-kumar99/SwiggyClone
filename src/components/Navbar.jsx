382ce8c51be2e5a16a46860d29b17c3c768380f7
Amit-kumar99 <amit.kumar.16208932@gmail.com>
2024-01-02 21:37:51 +0530

    modified navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
 import { Link } from 'react-router-dom'
+import { LOGO_URL } from "../utils/constants";
 
 const Navbar = () => {
   return (
     <div className="border-b-2 shadow-md">
-        <div className="w-9/12 flex justify-between mx-auto my-7">
-          <img src="" alt="swiggy-logo"/>
-          <div>
-            <Link className='m-2' to="/">Home</Link>
-            <Link className='m-2' to="/signin">Sign In</Link>
-            <Link className='m-2' to="/signup">Sign Up</Link>
-            <Link className='m-2' to="/cart">Cart</Link>
+        <div className="w-9/12 flex justify-between mx-auto my-2">
+          <img className='w-16' src={LOGO_URL} alt="swiggy-logo"/>
+          <div className='my-auto font-semibold text-md text-gray-700 flex'>
+            <div className='border pr-2'>
+              <input className='p-2' type="text" placeholder='Search'/>
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </div>
+            <div className='ml-16 mt-2'>
+              <Link className='mx-5' to="/">Home</Link>
+              <Link className='mx-5' to="/signin">Sign In</Link>
+              <Link className='mx-5' to="/signup">Sign Up</Link>
+              <Link className='mx-5' to="/cart">Cart</Link>
+            </div>
           </div>
         </div> 
     </div>
