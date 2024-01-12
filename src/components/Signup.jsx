600f44bb6976921fc373ccc0c53b534a4ad2f428
Amit-kumar99 <amit.kumar.16208932@gmail.com>
2024-01-12 11:10:24 +0530

    used useRef for Signup i/p fields

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -3,29 +3,31 @@ import { Link, useNavigate } from "react-router-dom";
 
 const Signup = () => {
   //use firebase
-  const mobileNumber = useRef(null);
-  console.log(mobileNumber);
-  const name = useRef(null);
-  const email = useRef(null);
+  const mobileNumberRef = useRef(null);
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
 
-  console.log(ref);
   const [checked, setChecked] = useState(false);
 
   const navigate = useNavigate();
 
-  function validateFormInputs() {
-    if (mobileNumber.current.value.length === 10 && name.current.value.trim() !== "" && email.current.value.includes('@') && checked) {
-      return true;
-    } 
-    else {
-      return false;
-    }
+  const validateFormInputs = () => {
+    // if (mobileNumberRef.current.value.length === 10 && 
+    //     nameRef.current.value.trim() !== "" && 
+    //     emailRef.current.value.includes('@') && 
+    //     checked) {
+    //   return true;
+    // } 
+    // else {
+    //   return false;
+    // }
+    return true;
   }
 
-  // function handleSignup({mobileNumber, name, email}) {
-  //   navigate("/signin");
-  // }
   function handleSignup() {
+    console.log(mobileNumberRef.current.value);
+    console.log(nameRef.current.value);
+    console.log(emailRef.current.value);
     navigate("/signin");
   }
 
@@ -33,61 +35,39 @@ const Signup = () => {
     <div className="border border-pink w-4/12 mt-5 m-auto rounded-md bg-pink-100">
       <h2 className="text-4xl m-4">Sign up</h2>
       <form className="m-4" onSubmit={(e) => e.preventDefault()}>
-        <div>
-          <input
-            ref={mobileNumber}
-            className="border border-gray-400 p-3 rounded-sm w-full"
-            type="text"
-            placeholder="Mobile Number"
-          />
-          <div className="mb-2">
-            <label>
-              {mobileNumber.current.value.length !== 10 && <small className="text-red-500">Mobile number must be 10 digits</small>}
-            </label>
-          </div>
-        </div>
-        <div>
-          <input
-            ref={name}
-            className="border border-gray-400 p-3 rounded-sm w-full"
-            type="text"
-            placeholder="Name"
-            required
-          />
-          <div className="mb-2">
-            <label className="text-danger">
-              {name.current.value.trim() === "" && <small className="text-red-500">Name must not be empty</small>}
-            </label>
-          </div>
-        </div> 
-        <div>
-          <input
-            ref={email}
-            className="border border-gray-400 p-3 rounded-sm w-full"
-            type="email"
-            placeholder="Email"
-          />
-          <div className="mb-2">
-            <label className="text-danger">
-              {!email.current.value.includes("@") && <small className="text-red-500">Invalid email</small>}
-            </label>
-          </div>     
-        </div>
-        <div className="form-check mt-2">
-          <input
-            className="border border-black p-1 my-1"
-            onChange={(e) => setChecked(e.target.checked)}
-            id="checkbox"
-            type="checkbox"
-          />
-          <label className="form-check-label" for="checkbox">
-            I agree to Burgeria's Terms of Service and Privacy Policy.
-          </label>
-        </div>
+        <input
+          ref={mobileNumberRef}
+          className="border border-gray-400 p-3 my-2 rounded-sm w-full"
+          type="text"
+          placeholder="Mobile Number"
+        />
+        <input
+          ref={nameRef}
+          className="border border-gray-400 p-3 my-2 rounded-sm w-full"
+          type="text"
+          placeholder="Name"
+          required
+        />
+        <input
+          ref={emailRef}
+          className="border border-gray-400 p-3 my-2 rounded-sm w-full"
+          type="email"
+          placeholder="Email"
+        />   
+        <input
+          className="border border-black p-1 my-1"
+          onChange={(e) => setChecked(e.target.checked)}
+          id="checkbox"
+          type="checkbox"
+        />
+        <label className="form-check-label" >
+          I agree to Burgeria's Terms of Service and Privacy Policy.
+        </label>
         <button
           disabled={!validateFormInputs()}
           className="bg-green-500 p-3 my-2 rounded-md w-full text-white"
-          onClick={() => handleSignup({mobileNumber, name, email})}
+          // onClick={() => handleSignup({mobileNumber, name, email})}
+          onClick={handleSignup}
         >
           Create account
         </button>
