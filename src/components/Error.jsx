6f6f044dbc382fbc920a883a3489cde82201112e
Amit-kumar99 <amit.kumar.16208932@gmail.com>
2024-03-29 12:46:03 +0530

    refactored Error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,11 +3,11 @@ import { useRouteError } from "react-router-dom";
 const Error = () => {
   const error = useRouteError();
   return (
-    <div>
-      <h1>Oops!!!</h1>
-      <h2>Something went wrong!!</h2>
-      <h3>
-        {error.status}: {error.statusText}
+    <div className="w-[20vw] mx-auto pt-[10vh]">
+      <h1 className="text-5xl text-red-500">Oops!!!</h1>
+      <h2 className="text-3xl pt-2">Something went wrong!!</h2>
+      <h3 className="pt-2">
+        <span className="text-2xl text-red-500">{error.status}</span> {error.statusText}
       </h3>
     </div>
   );
