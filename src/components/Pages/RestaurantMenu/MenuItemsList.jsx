9b9587ed2e02eb9e0bba3d2dfe325369ffa939a3
Amit-kumar99 <amit.kumar.16208932@gmail.com>
2024-11-14 13:21:47 +0530

    complex feature: on clicking ADD+ button, the button changes to showing the no. of items in the cart eg: - 1 +, & you can increase or decrease items.

--- src/components/Pages/RestaurantMenu/MenuItemsList.jsx
+++ src/components/Pages/RestaurantMenu/MenuItemsList.jsx
@@ -1,39 +1,82 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { MENU_ITEMS_IMG_API } from "../../../utils/constants";
-import { addItems } from "../../../utils/cartSlice";
+import { addItems, removeItems } from "../../../utils/cartSlice";
+import { useState } from "react";
 
-const MenuItemsList = ({menuItems}) => {
-    console.log(menuItems[0]);
-    const dispatch = useDispatch();
+const MenuItemsList = ({ menuItems }) => {
+  const dispatch = useDispatch();
+  const cartItemsList = useSelector((store) => store.cart.cartItems);
+  const countOfAllCartItems = useSelector(
+    (store) => store.cart.countOfAllCartItems
+  );
 
-    const handleAddItems = ({item}) => {
-        dispatch(addItems(item));
-    }
+  const handleSubtractItem = ({ item }) => {
+    dispatch(removeItems(item));
+  };
 
-    return (
-        <div>
-            {menuItems && menuItems.map((item)=>(
-                <div className="flex border-b-2 p-4 border-gray-300 justify-between" key={item.card.info.id}>
-                    <div className="p-2 w-9/12"> 
-                    <p>{item.card.info.name}</p>
-                    <p>₹{item.card.info.price / 100}</p>
-                    <p className="font-normal text-gray-500 text-xs">{item.card.info.description}</p>
-                    </div>
-                    <div>
-                        <div className="border border-gray-400 absolute bg-white rounded-md shadow-lg py-1 px-2 text-lime-500">
-                            <button 
-                                className="pb-1 font-semibold"
-                                onClick={() => handleAddItems({item})}>    
-                                    ADD +
-                            </button>
-                        </div>
-                        <img className="w-40 rounded-md"
-                        src={MENU_ITEMS_IMG_API + item.card.info.imageId} />
-                    </div>
+  const handleAddItem = ({ item }) => {
+    dispatch(addItems(item));
+  };
+
+  return (
+    <div>
+      {menuItems &&
+        menuItems.map((item) => {
+          const cartItemIndex = cartItemsList.findIndex(
+            (cartItem) => item.card.info.id === cartItem.card.info.id
+          );
+
+          return (
+            <div
+              data-testid="accordionItem"
+              className="flex border-b-2 p-4 border-gray-300 justify-between"
+              key={item.card.info.id}
+            >
+              <div className="p-2 w-9/12">
+                <p>{item.card.info.name}</p>
+                <p>₹{item.card.info.price / 100}</p>
+                <p className="font-normal text-gray-500 text-xs">
+                  {item.card.info.description}
+                </p>
+              </div>
+              <div>
+                <div className="border border-gray-400 absolute bg-white rounded-md shadow-lg py-1 px-2 text-lime-500">
+                  <div className="pb-1 font-semibold">
+                    {cartItemIndex === -1 ? (
+                      <button onClick={() => handleAddItem({ item })}>
+                        ADD +
+                      </button>
+                    ) : (
+                      <div>
+                        <button
+                          className="pr-2"
+                          onClick={() => handleSubtractItem({ item })}
+                        >
+                          -
+                        </button>
+                        <span className="px-2 cursor-default">
+                          {countOfAllCartItems[cartItemIndex]}
+                        </span>
+                        <button
+                          className="pl-2"
+                          onClick={() => handleAddItem({ item })}
+                        >
+                          +
+                        </button>
+                      </div>
+                    )}
+                  </div>
                 </div>
-            ))}
-        </div>
-    )
-}
+                <img
+                  className="w-40 rounded-md"
+                  src={MENU_ITEMS_IMG_API + item.card.info.imageId}
+                />
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
 
 export default MenuItemsList;
