b80a79a2de54abc1f63092fcc97327767460111e
Amit-kumar99 <amit.kumar.16208932@gmail.com>
2024-11-17 10:41:24 +0530

    modified footer

--- src/components/Pages/Home/Footer.jsx
+++ src/components/Pages/Home/Footer.jsx
@@ -2,14 +2,19 @@ const Footer = () => {
   return (
     <div className="bg-black mt-10">
       <div className="w-7/12 flex text-white justify-between m-auto">
-          <div className="my-10">
-              <h1 className="font-semibold mb-2 text-xl">Swiggy</h1>
-          </div>
-          <div className="my-10 text-lg">Contact</div>
-          <div className="my-10 text-lg">Delivery Locations</div>
+        <div className="my-10">
+          <h1 className="font-semibold mb-2 text-xl">Zwiggy</h1>
+        </div>
+        <div className="my-10 text-lg">
+          <div className="font-semibold">Delivery Locations</div>
+          <div>Hyderabad</div>
+        </div>
+      </div>
+      <div className="text-white flex justify-center mx-auto pb-10 text-2xl">
+        Developed with ❤️ by Amit Kumar
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
